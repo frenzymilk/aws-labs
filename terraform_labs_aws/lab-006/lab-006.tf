6c9b0c29a5ef5de40c920e23df95448afd59a99d
Pernelle M <pernelle.mensah@gmail.com>
2024-02-29 11:22:46 -0500

    Updating security group

--- terraform_labs_aws/lab-006/lab-006.tf
+++ terraform_labs_aws/lab-006/lab-006.tf
@@ -96,10 +96,10 @@ resource "aws_security_group" "remote_http_access_sg" {
   vpc_id      = aws_vpc.lab_006_vpc.id
 
   ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
